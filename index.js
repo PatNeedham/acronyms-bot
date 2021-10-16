a2d23a1d94132be954cdaca05a04fb033269b4ad
Pat Needham <pat@needham.io>
2021-10-15 22:03:38 -0400

    make graphql query to fetch matching terms

--- index.js
+++ index.js
@@ -1,15 +1,43 @@
 const axios = require('axios')
 const qs = require('qs')
-
+const appsync = require('aws-appsync')
+const gql = require('graphql-tag')
+require('cross-fetch/polyfill')
 require('dotenv').config()
+
+const graphqlClient = new appsync.AWSAppSyncClient({
+  url: 'https://eebl3ns3njh7plqngjeobimbrm.appsync-api.us-east-1.amazonaws.com/graphql',
+  region: process.env.AWS_REGION,
+  auth: {
+    type: 'AWS_IAM',
+    credentials: {
+      accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+      secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+      sessionToken: process.env.AWS_SESSION_TOKEN
+    }
+  },
+  disableOffline: true
+})
+
+const getAcronyms = gql`
+query getAcronyms($term: String!) {
+  listAcronymsData(filter: {acronym: {eq: $term}}) {
+    items {
+      acronym
+      description
+      meaning
+    }
+  }
+}
+`
 
 const apiUrl = 'https://slack.com/api'
 
-const postMessage = async (user, channel, text) => {
+const postMessage = async (channel, text) => {
   const args = {
     token: process.env.OAUTH_TOKEN,
     channel: channel,
-    text: `<@${user}> this is the text you entered: ${text}`
+    text
   }
   const result = await axios.post(`${apiUrl}/chat.postMessage`, qs.stringify(args))
   return result
@@ -31,6 +59,21 @@ exports.handler = async (event) => {
     return { statusCode: 200 }
   }
   try {
+    const result = await graphqlClient.query({ query: getAcronyms, variables: { term: text } })
+    const matches = result?.data?.listAcronymsData?.items
+    if (matches.length > 1) {
+      await postMessage(channel, `<@${user}> there were multiple matches found: ${matches.map(m => m.meaning).concat(', and ')}`)
+    }
+    if (matches.length === 1) {
+      await postMessage(channel, `<@${user}> ${text} stands for: ${matches[0].meaning}`)
+    }
+    if (matches.length === 0) {
+      await postMessage(channel, `<@${user}> no matching acronyms found for ${text}. Request one be added here`)
+    }
+    console.log('result from AppSync query:')
+    console.log(JSON.stringify(result, null, 2))
+    console.log('matching items:')
+    console.log(JSON.stringify(result?.data?.listAcronymsData?.items))
     await postMessage(user, channel, text)
   } catch (e) {
     console.log('error posting message:')
